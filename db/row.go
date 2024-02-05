93b54b35228c34e608e9bead55b21065f687ac30
Robert Bruce <entegral@protonmail.com>
2024-02-05 00:55:58 -0800

    approaching the row type from a more generic angle

---
+++ db/row.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+
+	"github.com/entegral/gobox/types"
+)
+
+type Keys struct {
+	Pk  string `dynamodbav:"pk,omitempty" json:"pk,omitempty"`
+	Sk  string `dynamodbav:"sk,omitempty" json:"sk,omitempty"`
+	Pk1 string `dynamodbav:"pk1,omitempty" json:"pk1,omitempty"`
+	Sk1 string `dynamodbav:"sk1,omitempty" json:"sk1,omitempty"`
+	Pk2 string `dynamodbav:"pk2,omitempty" json:"pk2,omitempty"`
+	Sk2 string `dynamodbav:"sk2,omitempty" json:"sk2,omitempty"`
+	Pk3 string `dynamodbav:"pk3,omitempty" json:"pk3,omitempty"`
+	Sk3 string `dynamodbav:"sk3,omitempty" json:"sk3,omitempty"`
+	Pk4 string `dynamodbav:"pk4,omitempty" json:"pk4,omitempty"`
+	Sk4 string `dynamodbav:"sk4,omitempty" json:"sk4,omitempty"`
+	Pk5 string `dynamodbav:"pk5,omitempty" json:"pk5,omitempty"`
+	Sk5 string `dynamodbav:"sk5,omitempty" json:"sk5,omitempty"`
+	Pk6 string `dynamodbav:"pk6,omitempty" json:"pk6,omitempty"`
+	Sk6 string `dynamodbav:"sk6,omitempty" json:"sk6,omitempty"`
+}
+
+type DBManagerInterface[T types.Keyable] interface {
+	Table(ctx context.Context) string
+	Get(ctx context.Context, input types.Keyable) (err error)
+	Put(ctx context.Context, input types.Keyable) (oldRow Row[T], err error)
+	Delete(ctx context.Context, row types.Keyable) (oldRow Row[T], err error)
+}
+
+type Row[T types.Keyable] struct {
+	Keys
+	Object    T
+	DBManager DBManagerInterface[T] `dynamodbav:"-" json:"-"`
+}
